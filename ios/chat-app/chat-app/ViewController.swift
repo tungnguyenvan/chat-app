4e3be337c7436d6151faf8d2d78a2351c0ebdd7d
Tùng Nguyễn <tungnguyen@Tungs-MacBook-Pro.local>
2019-07-23 19:38:31 +0700

    + create project

---
+++ ios/chat-app/chat-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  chat-app
+//
+//  Created by Tùng Nguyễn on 7/23/19.
+//  Copyright © 2019 Tùng Nguyễn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
