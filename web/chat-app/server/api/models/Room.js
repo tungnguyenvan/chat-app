9f91cca224ccd30a2eea0e8373020a817fecbef5
tung.nguyenvan <tung.nguyenvan@gameloft.com>
2019-07-26 16:26:24 +0700

    + Add type to room schema

--- web/chat-app/server/api/models/Room.js
+++ web/chat-app/server/api/models/Room.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const roomSchema = mongoose.Schema({
     _id:        mongoose.Types.ObjectId,
     name:       { type: String, required: true },
-    users:      { type: [String], required: true },
+    users:      [{ type: String }],
+    type:       { type: String, required: true }, // Type is chat or room
     avatar_url: { type: String, },
     create_at:  { type: Number },
     update_at:  { type: Number }
