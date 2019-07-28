2f511d9e1825762d62f8676316227be08de08650
Nguyễn Văn Tùng <nguyentungtungtungnguyen@gmail.com>
2019-07-28 20:52:26 +0700

    + Create and get room

--- web/chat-app/server/api/routers/Room.js
+++ web/chat-app/server/api/routers/Room.js
@@ -4,3 +4,46 @@ const mongoose  = require('mongoose');
 const bcrypt    = require('bcrypt');
 const jwt       = require('jsonwebtoken');
 
+const Room      = require('../models/Room');
+const checkToken = require('../middleware/AuthMiddleware');
+
+router.get('/', checkToken, (req, res, next) => {
+    Room.find().exec()
+    .then(result => {
+        return res.status(200).json({
+            result: result
+        });
+    })
+    .catch(err => {
+        return res.status(500).json({
+            error: err
+        });
+    });
+});
+
+router.post('/', (req, res, next) => {
+    const today = Date.now();
+    const room = new Room({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        users: req.body.users,
+        type: req.body.type,
+        avatar_url: req.body.avatar_url,
+        create_at: today,
+        update_at: today
+    });
+
+    room.save()
+    .then(result => {
+        return res.status(200).json({
+            result: result
+        });
+    })
+    .catch(err => {
+        return res.status(500).json({
+            error: err
+        });
+    });
+});
+
+module.exports = router;
