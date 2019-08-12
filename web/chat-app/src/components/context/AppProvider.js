7e21a7d0dc527ab3bb8e8e687c70563553528fa6
Nguyen Van Tung <tung.nguyenvan@gameloft.com>
2019-08-12 16:28:14 +0700

    + Update AppProvider

--- web/chat-app/src/components/context/AppProvider.js
+++ web/chat-app/src/components/context/AppProvider.js
@@ -4,6 +4,33 @@ const AppContext = React.createContext();
 class AppProvider extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            // Show progress bar
+            isShowProgressBar: true,
+            showProgressbar: this.showProgressbar(isShow),
+
+            // Token
+            userInfo:  {},
+            setUserInfo:   this.setUserInfo(user),
+        }
+
+        this.showProgressbar = this.showProgressbar.bind(this);
+        this.setUserInfo = this.setUserInfo.bind(this);
+    }
+
+    // Show or dimiss progress bar
+    showProgressbar(isShow) {
+        this.setState({
+            isShowProgressBar: isShow
+        });
+    }
+
+    // Set info user
+    setUserInfo(user) {
+        this.setState({
+            userInfo: user
+        });
     }
 
     render() {
